76e6f1ed19944fb6ec90257fc888b4977d6de0f9
Karl Broman <kbroman@gmail.com>
2019-06-03 10:55:57 -0500

    miniCRAN.R: versions 3.4,3.5,3.6; httr for GNapi

--- miniCRAN.R
+++ miniCRAN.R
@@ -6,9 +6,10 @@ library(miniCRAN)
 
 repos <- "https://cran.rstudio.com"
 
-pkgs <- c("Rcpp", "RcppEigen", "yaml", "jsonlite", "data.table",
+pkgs <- c("Rcpp", "RcppEigen", "yaml", "jsonlite", "data.table", "httr",
           "knitr", "rmarkdown", "qtl", "qtlcharts", "optparse", "RSQLite", "fst")
 pkgList <- pkgDep(pkgs, repos=repos, type="source", suggests=FALSE)
 
-makeRepo(pkgList, path="qtl2cran", repos=repos, type=c("source", "win.binary", "mac.binary.el-capitan"))
 makeRepo(pkgList, path="qtl2cran", repos=repos, type=c("win.binary", "mac.binary.el-capitan"), Rversion="3.4")
+makeRepo(pkgList, path="qtl2cran", repos=repos, type=c("win.binary", "mac.binary.el-capitan"), Rversion="3.5")
+makeRepo(pkgList, path="qtl2cran", repos=repos, type=c("source", "win.binary", "mac.binary.el-capitan"))
